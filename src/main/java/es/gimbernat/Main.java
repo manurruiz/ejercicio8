388d5faa11fb46678bc7eb29ada8e3582be1b32d
manu_ <ruizmanu2708@gmail.com>
2026-04-21 10:14:36 +0200

    Ejercicio 8 - modificado

--- src/main/java/es/gimbernat/Main.java
+++ src/main/java/es/gimbernat/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Partido partido = new Partido("R.Betis", "Sevilla");
+        Partido partido2 = new Partido("At.Madrid", "Madrid");
+
 
         Suscriptor s1 = new Suscriptor("Manu");
         Suscriptor s2 = new Suscriptor("Luis");
@@ -11,25 +13,39 @@ public class Main {
         AppEstadisticas stats = new AppEstadisticas();
 
         partido.suscribir(s1);
+        partido2.suscribir(s1);
+
         partido.suscribir(s2);
+        partido2.suscribir(s3);
+
         partido.suscribir(stats);
+        partido2.suscribir(stats);
 
         partido.iniciarPartido();
+        partido2.iniciarPartido();
         partido.golLocal();
+        partido2.golLocal();
 
         partido.suscribir(s3);
+        partido2.suscribir(s2);
 
         partido.golVisitante();
-        partido.descanso();
+        partido2.golVisitante();
 
+        partido.descanso();
+        partido2.descanso();
 
         partido.desuscribir(s2);
+        partido2.desuscribir(s2);
 
         partido.segundaParte();
+        partido2.segundaParte();
         partido.golLocal();
+        partido2.golLocal();
 
         partido.desuscribir(s1);
-
+        partido2.desuscribir(s1);
         partido.finalPartido();
+        partido2.finalPartido();
     }
 }
